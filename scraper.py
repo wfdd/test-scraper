af7dddd308f3881001a023b118d34c351886be08
wfdd <erdbanfed@gmail.com>
2019-01-31 15:03:01 +0200

    Let's see if aiohttp fails as well

--- scraper.py
+++ scraper.py
@@ -1,4 +1,16 @@
 
+import asyncio
 from urllib.request import urlopen
+
+from aiohttp import ClientSession
+
+
+async def test():
+    async with ClientSession() as session, \
+               session.get('https://ina.gl/inatsisartut/sammensaetning-af-inatsisartut/'):
+        pass
+
+asyncio.get_event_loop().run_until_complete(test())
+
 
 urlopen('https://ina.gl:443/inatsisartut/sammensaetning-af-inatsisartut/')
